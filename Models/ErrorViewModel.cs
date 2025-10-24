983db59565024c8ceeed2193f2790096070af330
chubbyboyzz <hieumit171805@gmail.com.>
2025-10-24 16:01:08 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PcSaler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
